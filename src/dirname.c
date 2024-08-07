655e4d073c2e6840260e6cbe4e81516cac4f7fae
Jake Glenn <jbradleyglenn@gmail.com>
2024-08-06 22:39:06 -0400

    dirname partial impl

--- src/dirname.c
+++ src/dirname.c
@@ -1,2 +1,41 @@
 // dirname -- strip suffix from file name
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+last_slash_index (char *name)
+{
+    int out = -1;
+    for (int i = 0; name[i]; i++)
+    {
+        if (name[i] == '/')
+        {
+            out = i;
+        }
+    }
+    return out;
+}
+
+int
+main (int argc, char **argv)
+{
+    int len;
+    char *out;
+    char dot = '.';
+    for (int i = 1; i < argc; i++)
+    {
+        out = argv[i];
+        len = last_slash_index (out);
+        
+        if (len == -1)
+        {
+            out = &dot;
+            len = 1;
+        }
+        
+        fwrite (out, 1, len, stdout);
+        putchar ('\n');
+    }
+    return EXIT_SUCCESS;
+}
